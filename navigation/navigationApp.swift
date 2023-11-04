f8de4602429833dd3c5bc30047ed37860581324d
Pavlo Semenchenko <imagemakerspa@icloud.com>
2023-11-04 19:39:27 +0200

    Initial Commit

---
+++ navigation/navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationApp.swift
+//  navigation
+//
+//  Created by Pavel Semenchenko on 04.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
